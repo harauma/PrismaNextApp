7a28927f6910b74d35da145874b5a1908d53d467
原　涼馬 <fot39hara@gmail.com>
2022-11-01 21:59:38 +0900

    Create temp Dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { LogoutIcon } from '@heroicons/react/solid'
 import { Layout } from '../components/Layout'
 
-const Dashboard = () => {
-  return <Layout title="Task Board">dashboard</Layout>
+const Dashboard: NextPage = () => {
+  const router = useRouter()
+  const logout = async () => {
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
+    router.push('/')
+  }
+  return (
+    <Layout title="Task Board">
+      <LogoutIcon
+        className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
+        onClick={logout}
+      />
+    </Layout>
+  )
 }
 
 export default Dashboard
